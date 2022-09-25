bd0a746ba595c872676e0d4e5af3b6dadb470692
pankaj <pankajkatre722@gmail.com>
2022-09-25 15:09:46 +0530

    first commit

---
+++ anime.js
@@ -0,0 +1,81 @@
+gsap.to(".image-container",{
+    ease:Expo.easeInOut,
+    width:"100%",
+    duration:2,
+    stagger:2
+})
+gsap.to("#main",{
+    ease:Expo.easeInOut,
+    duration:2,
+    stagger:2,
+    backgroundColor:"#ADACB3"
+})
+gsap.to("#main",{
+    ease:Expo.easeInOut,
+    duration:2,
+    delay:2,
+    stagger:2,
+    backgroundColor:"#BD9785"
+})
+gsap.to("#main",{
+    ease:Expo.easeInOut,
+    duration:2,
+    stagger:2,
+    delay:4,
+    backgroundColor:"#B68895"
+})
+gsap.to(".text h1",{
+    duration:2,
+    left:"0",
+    ease:Expo.easeInOut,
+    stagger:2,
+    // opacity:0
+})
+gsap.to(".text h1",{
+    delay:2,
+    duration:2,
+    left:"500px",
+    ease:Expo.easeInOut,
+    stagger:2,
+    opacity:0
+})
+gsap.to(".para p",{
+    duration:2,
+    top:"40%",
+    ease:Expo.easeInOut,
+    stagger:2,
+    // opacity:0
+})
+gsap.to(".para p",{
+    duration:2,
+    delay:2,
+    ease:Expo.easeInOut,
+    stagger:2,
+    opacity:0
+})
+gsap.to("#id img",{
+    duration:.6,
+    right:"0%",
+    ease:Expo.easeInOut,
+    stagger:.5,
+})
+gsap.to(".div-line .line",{
+    duration:2,
+    width:"100%",
+    ease:Expo.easeInOut,
+    stagger:2,
+    backgroundColor:""
+})
+gsap.to(".div-line .line",{
+    duration:2,
+    delay:2,
+    width:"200%",
+    opacity:0,
+    ease:Expo.easeInOut,
+    stagger:2
+})
+
+   
+
+
+
